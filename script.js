8547b7e4c8d61da61bb2a0f7b2e70e0fba33c155
ulusha <ummelkhatov@gmail.com>
2022-01-07 00:06:24 +0500

    last

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function random(min,max) {
   return Math.floor( Math.random() * (max - min) + min);
 }
 
+function old(){
   let run = Math.floor( Math.random() * 4);
   let gun = '=';
   if(run == 0){
@@ -13,15 +14,15 @@ function random(min,max) {
   }else if(run == 3){
     gun ='/';
   }
-
- 
+  return gun;
+}
 let number = +prompt('Введите количество примеров');
 
-for(let i=0;i<number;i++){
-  let number1 = random(20,30);
-  let number2 = random(1,20);
-  let primer = +prompt(number1 + gun + number2 + ' = ');
-  let answer = (number1+gun+number2) == primer ? 'молодец' : 'ошибка';    
-  alert(number1 + gun + number2 + ' = ' + (number1+gun+number2) + ' ваш ответ ' + primer + ' ! ' + answer);
+for(let i=0;i<number;i++) {
+  let number1 = random(10,20);
+  let number2 = random(1,10);
+  let primer = +prompt(number1 + old() + number2);
+  let answer = (number1+old()+number2) == primer ? 'молодец' : 'ошибка';    
+  alert(number1 + old() + number2 + ' = ' + (number1+old()+number2) + ' ваш ответ ' + primer + ' ! '+ answer);
 }
 
